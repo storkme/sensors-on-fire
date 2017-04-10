03ef3da583b23b1e94b94c91bf77b3141f387620
alex <haha go away>
2017-04-10 19:51:54 +0100

    welp

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ setInterval(() => {
     // filter stuff starting with 28
         .filter(whatever => whatever.substring(0, 2) === '28')
         // read the w1_slave file in that dir
-        .map(dir => fs.readFileSync(dir + '/w1_slave'))
+        .map(dir => fs.readFileSync('/sys/bus/w1/devices/' + dir + '/w1_slave', 'ascii'))
         // extract temp
         .map(contents => /t=(\d+)/.exec(contents))
         // filter out nulls
