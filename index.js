41c94ca6c0ed33b5e0b9cc384f8a027a11903dca
alex <haha go away>
2017-04-10 19:41:49 +0100

    initial

---
+++ index.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const admin = require("firebase-admin");
+let serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://isithotinhereorisitjustm-91e09.firebaseio.com/"
+});
+
+let db = admin.database();
+let ref = db.ref("temps");
+
+setInterval(() => {
+    let crap = fs.readdirSync('/sys/bus/w1/devices/')
+    // filter stuff starting with 28
+        .filter(whatever => whatever.substring(0, 2) === '28')
+        // read the w1_slave file in that dir
+        .map(dir => fs.readFileSync(dir + '/w1_slave'))
+        // extract temp
+        .map(contents => /t=(\d+)/.exec(contents))
+        // filter out nulls
+        .filter(e => e)
+        // parse temp as int, divide by 1000 to get centigrade
+        .map(t => parseInt(t) / 1000);
+
+    console.dir(crap);
+}, 1000);
---
+++ readme.md
@@ -0,0 +1,5 @@
+sensors on fire
+
+¯\_(ツ)_/¯
+
+[![Alt text for your video](https://img.youtube.com/vi/7p2oXumIzfo/0.jpg)](http://www.youtube.com/watch?v=7p2oXumIzfo)
